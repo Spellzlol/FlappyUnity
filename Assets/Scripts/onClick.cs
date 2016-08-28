ce822338d2fa522df67d5355f4ed8edd4529be6d
Spellz <spellz53@gmail.com>
2016-08-28 18:46:36 +0300

    first scene done

---
+++ Assets/Scripts/onClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class onClick : MonoBehaviour
+{
+	public void transition(int sceneId)
+	{
+		SceneManager.LoadScene(sceneId);
+	}
+}
